4009604d1743d25ee8878c6227a68c174eac3ab4
708-U <708.u.biz@gmail.com>
2020-03-01 23:36:02 +0000

    Add meat the team section

--- resources/views/bootstrap/complete-website-layout/home.blade.php
+++ resources/views/bootstrap/complete-website-layout/home.blade.php
@@ -163,4 +163,14 @@
         </div>
     </div>
 </div>
+
+{{-- Meet the team --}}
+<div class="container-fluid padding">
+    <div class="row welcome text-center">
+        <div class="col-12">
+            <h1 class="display-4">Meet the team</h1>
+        </div>
+        <hr>
+    </div>
+</div>
 @endsection

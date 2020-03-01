18fd418a82ef61f11c9f1d19b1979608db2043f7
708-U <708.u.biz@gmail.com>
2020-03-01 03:28:52 +0000

    Add bootstrap routing and alert samples view.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +15,10 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'bootstrap', 'name' => 'bootstrap.'], function () {
+    Route::group(['prefix' => 'components', 'name' => 'components.'], function () {
+        Route::view('/alert', 'bootstrap.components.alert');
+    });
 });

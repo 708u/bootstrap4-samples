385bd68f0cf8e542859f62f46b9655da9b59731d
708-U <708.u.biz@gmail.com>
2020-03-01 08:38:26 +0000

    Adjust mergin and padding to sample-card

--- resources/views/layouts/sample-card.blade.php
+++ resources/views/layouts/sample-card.blade.php
@@ -1,10 +1,14 @@
-<h2>{{ $header }}</h2>
+<h2 class="mt-4">{{ $header }}</h2>
 <div class="card">
     <div class="card-header">
         {{ $discription }}
     </div>
     <div class="card-body">
-        <p class="card-title bg-dark text-white rounded">html: {{ $cardTitle }}</p>
-        {{ $cardContents }}
+        <div class="px-2 py-2 mb-3 bg-dark text-white rounded">
+            {{ $cardTitle }}
+        </div>
+        <div class="px-2 py-2 bg-light rounded">
+            {{ $cardContents }}
+        </div>
     </div>
 </div>

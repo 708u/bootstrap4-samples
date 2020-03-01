67c366e004eb6dc07386fe5084f50a1a85e231f0
708-U <708.u.biz@gmail.com>
2020-03-01 08:12:40 +0000

    Add Card Component

---
+++ resources/views/layouts/card.blade.php
@@ -0,0 +1,10 @@
+<h2>{{ $header }}</h2>
+<div class="card">
+    <div class="card-header">
+        {{ $discription }}
+    </div>
+    <div class="card-body">
+        <p class="card-title bg-dark text-white rounded">{{ $cardTitle }}</p>
+        {{ $cardContents }}
+    </div>
+</div>

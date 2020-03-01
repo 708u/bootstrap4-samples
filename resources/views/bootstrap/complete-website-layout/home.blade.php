7ee1ef0273f5f080ce52d260d5f8e95f28c226d0
708-U <708.u.biz@gmail.com>
2020-03-01 10:41:17 +0000

    Add Header

--- resources/views/bootstrap/complete-website-layout/home.blade.php
+++ resources/views/bootstrap/complete-website-layout/home.blade.php
@@ -7,10 +7,34 @@
 @endsection
 @section('content')
 {{-- Navigation --}}
-<nav class="navbar navbar-epand-md navbar-light bg-light sticky-top">
-    <div class="container-fluid">
-        <a href="#" class="navbar-brand"><img src="img/log.png"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"></button>
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+    <a href="#" class="navbar-brand"><img src="{{ asset('img/logo.png') }}"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">About</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Service</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Team</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Connect</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+        </ul>
     </div>
+</div>
 </nav>
 @endsection

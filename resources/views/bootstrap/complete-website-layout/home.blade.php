f24047d8829904812ada461146ef145aed412ac9
708-U <708.u.biz@gmail.com>
2020-03-01 10:15:41 +0000

    Add img and setting of copy img to public from resource dir

--- resources/views/bootstrap/complete-website-layout/home.blade.php
+++ resources/views/bootstrap/complete-website-layout/home.blade.php
@@ -6,5 +6,11 @@
     Badge Samples
 @endsection
 @section('content')
-    hello!!!
+{{-- Navigation --}}
+<nav class="navbar navbar-epand-md navbar-light bg-light sticky-top">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand"><img src="img/log.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"></button>
+    </div>
+</nav>
 @endsection
